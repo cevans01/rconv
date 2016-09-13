0999888ca17aed14960a6089d25be69c245180ae
Chris Evans <cevans3326@gmail.com>
2016-09-12 21:12:03 -0400

    encoder seems to work

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,17 @@ pub fn test_conv() {
     println!("Test!");
 }
 
-pub fn encode(input: &Vec<u8>) -> Vec<u8>
-{
-    println!("In encode!");
-    input.clone();
-}
-
-
 pub struct Encoder {
     k:      u32,
     rate:   u32,
     polys:  Vec<u8>,
-    start_state:    u32,
+    start_state:    u8,
+    frame_size:     u32,
     partab: Vec<u32>
 }
 
 impl Encoder {
-    pub fn new(k: u32, rate: u32, polys: Vec<u8>, start_state: u32) -> Encoder {
+    pub fn new(k: u32, rate: u32, polys: Vec<u8>, start_state: u8, frame_size: u32) -> Encoder {
 
         assert_eq!( rate, polys.len() as u32 );
 
@@ -58,8 +52,29 @@ impl Encoder {
             rate: rate,
             polys: polys,
             start_state: start_state,
+            frame_size: frame_size,
             partab: partab
         }
     }
 
+    fn parity(&self, x: u8) -> u32 {
+        self.partab[x as usize]
+    }
+
+    pub fn encode(&self, input: &Vec<u8>) -> Vec<u8>
+    {
+        let mut output: Vec<u8> = vec![0; (self.frame_size * self.rate) as usize];
+        let mut my_state = self.start_state;
+        for i in 0..self.frame_size {
+            my_state = (my_state << 1) | (input[i as usize] & 1);
+            for j in 0..self.rate {
+                if self.parity(my_state & self.polys[j as usize]) == 0 {
+                    output[(i * self.rate + j) as usize] = 0;
+                } else {
+                    output[(i * self.rate + j) as usize] = 1;
+                }
+            }
+        }
+        output
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,6 @@ fn usage() {
     println!("Usage: main");
 }
 
-/*
-fn test_conv() {
-    println!("Test!");
-}
-*/
-
 fn main() {
 
     let args: Vec<_> = std::env::args().collect();
@@ -22,7 +16,9 @@ fn main() {
     }
 
     let input: Vec<u8> = vec![1, 0, 1, 1, 0, 0, 1, 0, 1];
-    let output = rconv::encode(&input);
+    let polys: Vec<u8> = vec![2,0];
+    let enc = rconv::Encoder::new(2, 2, polys, 0, input.len() as u32);
+    let output = enc.encode(&input);
 
     //println!("output is {}", output);
     for x in &output {
