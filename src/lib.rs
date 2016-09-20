5f9cae66a49158401003a54e8abea7f7490bb786
Chris Evans <chris.evans@azuresummit.com>
2016-09-19 21:26:01 -0400

    unit test fails

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
-
 pub fn test_conv() {
     println!("Test!");
 }
@@ -44,7 +37,7 @@ impl Encoder {
         }
 
         for i in 0..partab_len {
-            println!("partab[{0}] = {1}", i, partab[i]);
+            //println!("partab[{0}] = {1}", i, partab[i]);
         }
 
         Encoder {
@@ -78,3 +71,48 @@ impl Encoder {
         output
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use Encoder;
+    #[test]
+    fn voyager_code() {
+        //g0 = [1,1,1,1,0,0,1] and g1 = [1,0,1,1,0,1,1]
+        let polys: Vec<u8> = vec![109,71];
+        let input: Vec<u8> = vec![0,1,1,0,1,0,0,0,0,1,1,0,1,0,0,1];
+        //let input: Vec<u8> = vec![1, 0, 1, 1, 0, 0, 1, 0, 1];
+        let enc = Encoder::new(7, 2, polys, 3, input.len() as u32);
+        let test_output = enc.encode(&input);
+
+        let truth_output: Vec<u8> = vec![
+            0,0,1,1,0,1,0,1,1,1,0,1,1,0,0,1,1,1,1,0,1,0,0,1,1,1,0,1,1,0,1,0,0,1,1,0,0,0,0,0,0,1,1,1,0,0];
+
+        println!( "truth.len() = {}", truth_output.len() );
+        println!( "test.len()  = {}", test_output.len() );
+        assert_eq!( truth_output, test_output );
+    }
+
+
+    #[test]
+    fn simple_code() {
+        //g0 = [1,1,1,1,0,0,1] and g1 = [1,0,1,1,0,1,1]
+        let polys: Vec<u8> = vec![13,11];
+        let input: Vec<u8> = vec![1,0,1,1,0];
+        //let input: Vec<u8> = vec![1, 0, 1, 1, 0, 0, 1, 0, 1];
+        let enc = Encoder::new(4, 2, polys, 0, input.len() as u32);
+        let test_output = enc.encode(&input);
+
+        /*
+        let truth_output: Vec<u8> = vec![
+            0,0,1,1,0,1,0,1,1,1,0,1,1,0,0,1,1,1,1,0,1,0,0,1,1,1,0,1,1,0,1,0,0,1,1,0,0,0,0,0,0,1,1,1,0,0];
+            */
+        let truth_output: Vec<u8> = vec![1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,0];
+
+        println!( "truth.len() = {}", truth_output.len() );
+        println!( "test.len()  = {}", test_output.len() );
+        assert_eq!( truth_output, test_output );
+    }
+}
+
--- src/unit_test.rs
+++ src/unit_test.rs
@@ -1,7 +1,3 @@
-
-extern crate rconv;
-
-/*
 #[cfg(test)]
 mod tests {
     #[test]
@@ -20,31 +16,4 @@ mod tests {
         assert!( truth_output == test_output );
     }
 }
-*/
 
-
-fn usage() {
-    println!("Usage: main");
-}
-
-fn main() {
-
-    let args: Vec<_> = std::env::args().collect();
-
-
-    match args.len() {
-        1 => rconv::test_conv(),
-        _ => usage()
-    }
-
-    let input: Vec<u8> = vec![1, 0, 1, 1, 0, 0, 1, 0, 1];
-    let polys: Vec<u8> = vec![2,0];
-    let enc = rconv::Encoder::new(2, 2, polys, 0, input.len() as u32);
-    let output = enc.encode(&input);
-
-    //println!("output is {}", output);
-    for x in &output {
-        println!("x = {}", x);
-    }
-
-}
