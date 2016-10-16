4c46ad5cfeeeb469e7548a6899d212373c5f1ceb
Chris Evans <chris.evans@azuresummit.com>
2016-10-16 16:17:33 -0400

    creating a module for common convolutional codes

---
+++ src/codes.rs
@@ -0,0 +1,15 @@
+
+//extern crate rconv;
+
+//pub const gsm_conv_rach: super::Encoder = super::Encoder::new(2, 5, 14, vec![23, 33], 0, super::EncoderType::CONV_TERM_FLUSH);
+
+/*
+gsm_conv_rach: super::Encoder = 
+    super::Encoder{ n: 2, 
+                    k: 5, 
+                    len: 14,
+                    polys: [23, 33],
+                    start_state: 0,
+                    termination: super::EncoderType::CONV_TERM_FLUSH
+    };
+    */
